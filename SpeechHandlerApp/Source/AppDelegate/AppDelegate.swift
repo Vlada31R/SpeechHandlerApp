d15bf0b520c65a3491d2d1fb56947f01720509ac
Vlada Radchenko <vlada.radchenko@nixsolutions.com>
2020-05-10 10:40:37 +0300

    Setup folders

---
+++ SpeechHandlerApp/Source/AppDelegate/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  SpeechHandlerApp
+//
+//  Created by Vlada Radchenko on 5/10/20.
+//  Copyright © 2020 Vlada Radchenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
+
