403ad7594c1f5db0402280e8266a8cb2eb1b56e1
vlada.radchenko <>
2020-05-24 10:34:25 +0300

    setup UI

--- SpeechHandlerApp/Source/Flows/MainFlow/Controllers/ViewAudioViewController/ViewAudioViewController.swift
+++ SpeechHandlerApp/Source/Flows/MainFlow/Controllers/ViewAudioViewController/ViewAudioViewController.swift
@@ -23,6 +23,30 @@ class ViewAudioViewController: BaseViewController {
     @IBOutlet private weak var areNotTransformedLabel: UILabel!
     @IBOutlet private weak var transformButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupInitialUI()
+    }
+    
     @IBAction func didTapTransformButton(_ sender: UIButton) {
+    }
+}
+
+// MARK: - Private
+private extension ViewAudioViewController {
+    
+    func setupInitialUI() {
+        
+        title = trackModel.name
+        
+        descriptionLabel.isHidden = trackModel.description == nil
+        descriptionLabel.text = trackModel.description
+        
+        transformedTextLabel.isHidden = trackModel.text == nil
+        transformedTextLabel.text = trackModel.text
+        
+        areNotTransformedLabel.isHidden = trackModel.isTransformed
+        transformButton.isHidden = trackModel.isTransformed
     }
 }
